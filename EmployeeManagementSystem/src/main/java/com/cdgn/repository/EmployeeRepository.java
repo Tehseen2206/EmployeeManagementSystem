e3f64855e151500700344a20eeba6529edb945d9
Tehseen2206 <mdtehseenfathima@gmail.com>
2025-07-10 13:58:13 +0530

    version 1

---
+++ EmployeeManagementSystem/src/main/java/com/cdgn/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.cdgn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cdgn.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+	boolean existsByEmailAndPassword(String email, String password);
+	Employee findByEmail(String email);
+    
+}
---
+++ EmployeeManagementSystem/src/main/java/com/cdgn/service/EmployeeService.java
@@ -0,0 +1,39 @@
+package com.cdgn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cdgn.model.Employee;
+import com.cdgn.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+    @Autowired
+	private EmployeeRepository repository;
+
+	public Employee saveEmployee(Employee employee) {
+		return repository.save(employee);
+	}
+
+	public boolean existsByEmailAndPassword(String email, String password) {
+		return repository.existsByEmailAndPassword(email,password);
+	}
+
+	public List<Employee> viewALLEmployees() {
+		return repository.findAll();
+	}
+
+	public Employee getByEmail(String email) {
+		return repository.findByEmail(email);
+	}
+
+	public void deleteById(int id) {
+		repository.deleteById(id);
+	}
+
+	public Employee findByEmail(String email) {
+		return repository.findByEmail(email);
+	}
+}
